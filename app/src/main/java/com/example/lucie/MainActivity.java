d73a7cbd08ccc30c7475eb80f288efcde60079eb
PATO <mupatt@gmail.com>
2020-02-14 15:23:40 +0300

    My second

---
+++ app/src/main/java/com/example/lucie/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.lucie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.lucie.DisplayMessageActivity;
+import com.example.lucie.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view){
+        EditText message = (EditText)findViewById(R.id.message);
+        Toast.makeText(this, "sending message" +message.getText().toString(),Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        intent.putExtra("MESSAGE", message.getText().toString());
+        startActivity(intent);
+        message.setText("");
+    }
+}
