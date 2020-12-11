b47ac5144d1e2ed4f8c744490ceeb290aa0ba1a9
Lucy <primahamutuh63@gmail.com>
2020-12-11 16:58:54 +0300

    Service, Internal and External

--- app/src/main/java/com/example/lucie/MainActivity.java
+++ app/src/main/java/com/example/lucie/MainActivity.java
@@ -13,11 +13,16 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -26,10 +31,19 @@ import android.widget.Toast;
 import com.example.lucie.DisplayMessageActivity;
 import com.example.lucie.R;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnsave;
+    EditText etinput;
+    String filename="";
+    String filepath="";
+    String filecontent="";
+
     public static String google = "http:/google.com";
     public static String gmail = "http:/gmail.com";
     TextView play;
@@ -50,6 +64,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
         registerReceiver(myReceiver,new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
         TextView textView=findViewById(R.id.textView);
 
@@ -62,6 +79,44 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
             }
         });
+
+        btnsave=findViewById(R.id.btnsave);
+        etinput=findViewById(R.id.etinput);
+        filename="myFile.text";
+        filepath="MyFileDir";
+        if(isExternalStorageAvailableForRW()){
+            btnsave.setEnabled(true);
+        }
+        btnsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                filecontent=etinput.getText().toString().trim();
+                if (!filecontent.equals("")){
+                    File myExternalFile=new File(getExternalFilesDir(filepath),filename);
+                    FileOutputStream fos=null;
+                    try {
+                        fos=new FileOutputStream(myExternalFile);
+                        fos.write(filecontent.getBytes());
+                    }catch (FileNotFoundException e){
+                        e.printStackTrace();
+
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+                    etinput.setText("");
+                    Toast.makeText(getApplicationContext(),"saved on the External Memory",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(getApplicationContext(),"Text field can't be empty",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    public boolean isExternalStorageAvailableForRW(){
+        String extStorage=Environment.getExternalStorageState();
+        if(extStorage.equals(Environment.MEDIA_MOUNTED)){
+            return  true;
+        }
+        return false;
     }
 
     public void sendMessage(View view) {
@@ -165,5 +220,44 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(items);
 
+    }
+
+    public void saveimage(View view){
+        saveReceivedImage("image");
+    }
+    private void saveReceivedImage(String imageName){
+        try {
+//            Bitmap bitmap = BitmapFactory.decodeByteArray(imageByteArray, 0, numberOfBytes);
+            Drawable drawable= ContextCompat.getDrawable(MainActivity.this,R.drawable.ic_launcher_background);
+            Bitmap bitmap=Bitmap.createBitmap(drawable.getIntrinsicWidth(),drawable.getIntrinsicHeight(),Bitmap.Config.ARGB_8888);
+
+            File path = new File(MainActivity.this.getFilesDir(), "AndroidProject" + File.separator + "Images");
+            if(!path.exists()){
+                path.mkdirs();
+            }
+            File outFile = new File(path, imageName + ".jpeg");
+            FileOutputStream outputStream = new FileOutputStream(outFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+            outputStream.close();
+            Toast.makeText(this, "image stored on internal memory ", Toast.LENGTH_SHORT).show();
+
+        } catch (FileNotFoundException e) {
+            Log.e("msg", "Saving received message failed with", e);
+            Toast.makeText(this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Toast.makeText(this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void creatBitMap() {
+
+        File Med = new File(Environment.getExternalStorageDirectory(), "myDirName");
+        if (!Med.exists()) {
+            if (Med.mkdir()) {
+                Toast.makeText(this, "Directory Created", Toast.LENGTH_SHORT).show();
+            } else {
+                Log.d("App", "Err");
+            }
+        }
     }
 }
